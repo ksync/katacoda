9607018bb91b32645872d59801918e89e3e1948d
Thomas Rampelberg <thomas@saunter.org>
2018-02-13 09:49:05 -0600

    sigh

--- 4/assets/launch.sh
+++
@@ -1,8 +0,0 @@
-echo "Starting Kubernetes..."
-while [ ! -f /opt/katacoda-completed ]
-do
-  sleep 1
-done
-
-sleep 2
-echo "Kubernetes Started"
--- 4/env-init.sh
+++
@@ -1,3 +0,0 @@
-~/.bin/minikube start
-kubectl run kubernetes-bootcamp --image=docker.io/jocatalin/kubernetes-bootcamp:v1 --port=8080
-echo "done" >> /opt/katacoda-completed
